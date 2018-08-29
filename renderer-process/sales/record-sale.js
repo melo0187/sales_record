8c75754b5ab024de3da8a86f55b2a2d8adce9cc1
Carmelo Scollo <carmelo.scollo@gmail.com>
2018-08-29 17:48:34 +0200

    Add onSubmit validation logic to form

--- renderer-process/sales/record-sale.js
+++ renderer-process/sales/record-sale.js
@@ -1,5 +1,7 @@
 const {ipcRenderer} = require('electron')
 
+const validator = require('validator')
+
 const remindInSelection = document.getElementById('remind-in')
 const remindYesRadioBtn = document.getElementById('yes')
 
@@ -46,16 +48,21 @@ function setupSaveBtn () {
   saveBtn.addEventListener('click', (event) => {
     event.preventDefault()
 
-    const salesRecord = {
-      lastName: lastNameInput.value,
-      firstName: firstNameInput.value,
-      email: emailInput.value,
-      address: addressTextArea.value,
-      soldItem: soldItemInput.value,
-      salesDate: salesDateInput.value,
-      remindInXMonths: remindYesRadioBtn.checked ? remindInSelection.value : 0
+    if (isFormValid()) {
+      const salesRecord = {
+        lastName: lastNameInput.value,
+        firstName: firstNameInput.value,
+        email: emailInput.value,
+        address: addressTextArea.value,
+        soldItem: soldItemInput.value,
+        salesDate: salesDateInput.value,
+        remindInXMonths: remindYesRadioBtn.checked ? remindInSelection.value : 0
+      }
+      ipcRenderer.send('sales:record', salesRecord)
+    } else {
+      // ToDo: Handle invalid form state
+      console.log('Form is invalid!')
     }
-    ipcRenderer.send('sales:record', salesRecord)
   })
 
   ipcRenderer.on('sales:recorded', (event, arg) => {
@@ -63,4 +70,53 @@ function setupSaveBtn () {
 
     // ToDo: Give feedback like clearing the form and stating that the sale has been recorded
   })
+}
+
+function isFormValid () {
+  const form = document.querySelector('form')
+
+  let valid = true
+
+  // Check if any of the required fields is empty
+  const requiredFields = form.querySelectorAll('input[required]')
+  for (let field of requiredFields) {
+    const input = validator.trim(field.value + '')
+    const isNotEmpty = !validator.isEmpty(input)
+    console.log(`Input for ${field.name} was ${isNotEmpty ? input : 'empty'}`)
+    valid = valid && isNotEmpty
+    console.log(`validity of form because of fields checked so far: ${valid}`)
+  }
+
+  // Next check that the text inputs match the requested pattern
+  const textFields = form.querySelectorAll('input[type="text"][pattern]')
+  for (let field of textFields) {
+    const input = validator.trim(field.value + '')
+    const pattern = field.pattern
+    const isMatch = validator.matches(input, pattern)
+    console.log(`${field.name}'s input ${isMatch ? 'is a match' : 'is no match'} for type="text" and pattern ${pattern}`)
+    valid = valid && isMatch
+    console.log(`validity of form because of fields checked so far: ${valid}`)
+  }
+
+  // And check that the e-mail input is an actual email, if provided
+  const emailFields = form.querySelectorAll('input[type="email"]')
+  for (let field of emailFields) {
+    const input = validator.trim(field.value + '')
+    const isEmailOrEmpty = (validator.isEmpty(input) || validator.isEmail(input))
+    console.log(`E-Mail input is ${isEmailOrEmpty ? 'fine (including empty)' : 'not an E-Mail'}`)
+    valid = valid && isEmailOrEmpty
+    console.log(`validity of form because of fields checked so far: ${valid}`)
+  }
+
+  // Finally verify that the date is valid (excluding future)
+  const dateFields = form.querySelectorAll('input[type="date"')
+  for (let field of dateFields) {
+    const input = field.value + ''
+    const isDateAndNotFuture = validator.toDate(input) !== null && !validator.isAfter(input)
+    console.log(`Sale's date input ${isDateAndNotFuture ? 'is valid date' : 'is no valid date in the past'}`)
+    valid = valid && isDateAndNotFuture
+    console.log(`validity of form because of fields checked so far: ${valid}`)
+  }
+
+  return valid
 }
